945a57d17fe701951c3b88f351b267b8c99ac000
Michel Santos <michel.cxs@gmail.com>
2021-10-29 09:30:01 -0300

    defining startup logic and scripts

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController{
+  public function index(){
+    echo "cheguei na indexController e disparamos a action index";
+  }
+
+  public function sobreNos(){
+    echo "cheguei na indexController e disparamos a action sobre nós";
+  }
+}
+
+?> 
---
+++ vendor/MF/Init/Bootstrap.php
@@ -0,0 +1,41 @@
+<?php
+//Termo utilizado para scripts de inicialização, métodos relativos ao framework.
+namespace MF\Init;
+
+abstract class Bootstrap {
+
+  private $routes;
+
+  abstract protected function initRoutes();
+
+  public function __construct() {
+    $this->initRoutes();
+    $this->run($this->getUrl());
+  }
+
+  public function getRoutes() {
+    return $this->routes;
+  }
+
+  public function setRoutes($routes) {
+      $this->routes = $routes;
+  }
+
+  protected function run($url) {
+    array_walk($this->routes, function($route) use ($url) {
+      if($url == $route['route']) {
+        $class = "App\\Controllers\\" . ucfirst($route['controller']);
+        $controller = new $class;
+        $action = $route['action'];
+        $controller->$action();
+      }
+    });
+  }
+
+  protected function getUrl() {
+    return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+  }
+
+}
+
+?>
